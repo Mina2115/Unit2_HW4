7a2991d96c786d244d8176600616df5acecb7d2d
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-02-22 19:59:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/ImageryLayer"
+      ], function (Map, MapView, ImageryLayer) {
+        /********************
+         * Create image layer
+         ********************/
+
+   /*     var layer = new ImageryLayer({
+          url:
+            "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NLCDLandCover2001/ImageServer",
+          format: "jpgpng" // server exports in either jpg or png format
+        }); */
+
+          var layer = new ImageryLayer({
+          url:
+            "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
+          format: "jpgpng" // server exports in either jpg or png format
+        });
+  
+        /**************************
+         * Add image layer to map
+         *************************/
+
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "gray-vector",
+          layers: [layer]
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
+          center: [-105.5, 36.15],
+          zoom: 4
         });
       });
