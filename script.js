7def972ec0e7c970cb26bf776bbae848c2a7b31f
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-02-27 09:57:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 require([
         "esri/Map",
         "esri/views/MapView",
-        "esri/layers/ImageryLayer"
-      ], function (Map, MapView, ImageryLayer) {
+        "esri/layers/ImageryLayer",
+  "esri/layers/support/RasterFunction"
+      ], function (Map, MapView, ImageryLayer, RasterFunction) {
         /********************
-         * Create image layer
+         * Set up popup template of image layer
          ********************/
 
-   /*     var layer = new ImageryLayer({
-          url:
-            "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NLCDLandCover2001/ImageServer",
-          format: "jpgpng" // server exports in either jpg or png format
-        }); */
+    var imagePopupTemplate = { // autocastes as neww PopupTemplate ()
+      Title: "Data from {SensorName} satellite",
+     Content: "Rendered NDVI values: <b>{Raster.ServicePixelValue} </b>" +
+            "<br>Original values (B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11): <b>{Raster.ItemPixelValue} </b>"
+        };
+  /*******************************************************************
+         * Create image layer with server defined raster function templates
+         ******************************************************************/
+  var serviceRFT = new RasterFunction({
+          functionName: "NDVI Raw",
+          variableName: "Raster"
+        });
 
           var layer = new ImageryLayer({
           url:
             "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
-          format: "jpgpng" // server exports in either jpg or png format
+           renderingRule: serviceRFT,
+          popupTemplate: imagePopupTemplate
         });
   
         /**************************
@@ -24,14 +33,18 @@ require([
          *************************/
 
         var map = new Map({
-          basemap: "gray-vector",
+          basemap: "hybrid",
           layers: [layer]
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          center: [-105.5, 36.15],
-          zoom: 4
+          center: [-90.1994, 38.6270],
+          zoom: 11,
+           popup: {
+            actions: []
+          }
         });
       });
+
